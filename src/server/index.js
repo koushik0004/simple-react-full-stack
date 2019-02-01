ed5ff32959a85582274375c0c7e610b3455d0acc
Koushik Sadhukhan <kousadhu@publicisgroupe.net>
2019-02-01 10:31:06 +0530

    initial server client port setup

--- src/server/index.js
+++ src/server/index.js
@@ -5,4 +5,4 @@ const app = express();
 
 app.use(express.static('dist'));
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
-app.listen(8080, () => console.log('Listening on port 8080!'));
+app.listen(1111, () => console.log('Listening on port 8080!'));
